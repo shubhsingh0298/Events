edcafa6bae0dbf181026378bf916df8fe74a1f77
shubhsingh0298 <147821801+shubhsingh0298@users.noreply.github.com>
2024-06-06 15:07:51 +0530

    Update MobileviewPW.py

--- MobileviewPW.py
+++ MobileviewPW.py
@@ -1,9 +1,5 @@
 import time
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
 from EventAutomationBaseClass import EventAutomation
-from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
