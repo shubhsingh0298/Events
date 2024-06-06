ab04e52bbea804cfc7f761b8437c6e0c043a3eff
shubhsingh0298 <147821801+shubhsingh0298@users.noreply.github.com>
2024-06-06 15:09:25 +0530

    Update MobileviewPW.py

--- MobileviewPW.py
+++ MobileviewPW.py
@@ -2,7 +2,6 @@ import time
 from EventAutomationBaseClass import EventAutomation
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 options = Options()
 options.add_argument("--disable-extensions")
