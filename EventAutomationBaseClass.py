721bfdd55dc0d3936c9892aaf179d89212615a78
shubhsingh0298 <147821801+shubhsingh0298@users.noreply.github.com>
2024-06-06 15:21:18 +0530

    Update EventAutomationBaseClass.py

--- EventAutomationBaseClass.py
+++ EventAutomationBaseClass.py
@@ -4,6 +4,10 @@ import json
 from datetime import datetime
 import csv
 from datetime import date
+options = Options()
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 
 class EventAutomation:
 
