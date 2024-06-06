90584d9163acdb55577bd6af4a78cd8b80c619e5
shubhsingh0298 <147821801+shubhsingh0298@users.noreply.github.com>
2024-06-06 15:15:31 +0530

    Update EventAutomationBaseClass.py

--- EventAutomationBaseClass.py
+++ EventAutomationBaseClass.py
@@ -1,7 +1,6 @@
 import time
 import json
 # from passcode import password
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import TimeoutException, NoSuchElementException, StaleElementReferenceException, \
